466d1180353a4e725fb84c67edd72cb0c0fe3399
Phuc <dovinhphuchappy@gmail.com>
2026-03-31 20:49:27 +0700

    ROUTES, SCH

---
+++ schemas/user.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const User = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullname: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        default: 'Chưa biết',
+    },
+    dob: {
+        type: String,
+        default: '',
+    },
+    avatar: {
+        type: String,
+        default: '',
+    },
+    role:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Role',
+        required: true
+    }
+}, {
+    timestamps: true
+});
+module.exports = mongoose.model('User', User)
